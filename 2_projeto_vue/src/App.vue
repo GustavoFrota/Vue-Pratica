f675245b7e980331954f2aa58782d5b4d33a5549
GustavoFrota <gustavofro90@gmail.com>
2026-03-30 10:26:43 -0300

    LifeCycle

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -2,16 +2,19 @@
  <div>
   <PrimeiroComponente />
   <p>O dado do epp é {{ testando }}</p>
+  <LifeCycle />
   </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
 
 export default {
   name: "app",
   components: {
-    PrimeiroComponente
+    PrimeiroComponente,
+    LifeCycle
   },
   data() {
     return {
