bbc241a7bc958ce6c614a67decb7e21cc0f9ca04
GustavoFrota <gustavofro90@gmail.com>
2026-04-01 09:23:07 -0300

    Pequena alteração

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -9,7 +9,7 @@
         <li>Python</li>
     </ul>
     <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
-    <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+    <p>Para acessar meu portfólio <a :href="meu_link" target="_blank">basta clicar aqui</a></p>
     </div>
 </template>
 
