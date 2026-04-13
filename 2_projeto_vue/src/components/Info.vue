b513dcce0803d143b6d9cc5d3aef27ff2a8c5f3a
GustavoFrota <gustavofro90@gmail.com>
2026-04-13 10:28:56 -0300

    Outra forma de usar v-for

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -2,9 +2,13 @@
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades</p>
-    <p>Utilizando as seguintes tecnologias para back-end:</p>
-    <ul v-for="technology, index in backend_tecnologies" v-bind:key="index">
-        <li> {{ technology }}</li>
+    <p>Utilizo as seguintes tecnologias para back-end:</p>
+    <ul>
+        <li v-for="technology, index in backend_tecnologies" v-bind:key="index"> {{ technology }}</li>
+    </ul>
+    <p>Utilizo as seguintes tecnologias para front-end:</p>
+    <ul>
+        <li v-for="technology in frontend_tecnologies" :key="technology.id"> {{ technology.language}}</li>
     </ul>
     <div>
         <button @click="showEmail"> {{ textoBotao }}</button>
@@ -24,7 +28,12 @@ export default {
             email: "gustavo@gmail.com",
             meu_link: "https://google.com",
             textoBotao: "Mostrar e-mail",
-            backend_tecnologies: ['Javascript', 'PHP', "Python"]
+            backend_tecnologies: ['Javascript', 'PHP', "Python"],
+            frontend_tecnologies: [
+                {id: 1, language: 'HTML'},
+                {id: 2, language: 'CSS'},
+                {id: 3, language: 'Vue'}
+            ]
         }
     },
     methods: {
